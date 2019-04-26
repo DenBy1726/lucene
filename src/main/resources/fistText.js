9c5c15e822e89c70e4e3d1b132a558756994f31b
dnal <realdenby@gmail.com>
2019-04-26 13:58:31 +0300

    change fish test data to real wiki articles

--- src/main/resources/fistText.js
+++ src/main/resources/fistText.js
@@ -1,13 +1,12 @@
-//script to generation fish text and convert it to sql script
-//just execute it in chrome
-let result = await Promise.all(new Array(100).fill(0).map(async x=> {
-    const randomDate = (start, end) =>  new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+(await Promise.all(new Array(50).fill(0).map(async x=>{
+    let randomTopics = await fetch("https://ru.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&generator=random&grnlimit=20&grnnamespace=0").then(x => x.json());
+    let page = Object.entries(randomTopics.query.pages).map(x=>x[1]).filter(x => x.extract !== undefined).map(x=> {
+        const randomDate = (start, end) =>  new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     const date  = randomDate(new Date(2012, 0, 1), new Date()).toLocaleDateString();
     const time  = randomDate(new Date(2012, 0, 1), new Date()).toLocaleTimeString();
-    const hTitle = await fetch("https://fish-text.ru/get?format=html&type=title&self=true").then(x => x.text().then(x=>x))
-    const hText  = await fetch("https://fish-text.ru/get?format=html&type=sentence&number=6.00&self=true").then(x => x.text().then(x=>x))
-    const title = hTitle.split('>')[1].split('<')[0]
-    const text = hText.split('>')[1].split('<')[0]
+    const title = x.title;
+    const text = x.extract;
     return `INSERT INTO public.document(title, text, date, time) VALUES ('${title}','${text}','${date}','${time}');`
-}));
-result.join('\n')
+});
+    return [...page]
+}))).flat().join('\n');
